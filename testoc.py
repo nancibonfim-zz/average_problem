8e62ded74a03b5c50b19f2603652bf9fd0eb55da
Nanci Bonfim <nancibonfim@gmail.com>
2018-11-07 14:11:37 -0300

    Removing old function

--- testoc.py
+++ testoc.py
@@ -1,15 +1,7 @@
-def average(filename):
-    # average of integers in the file
-    with open(filename, 'r') as f:
-        numbers = f.readlines()
-
-    total = sum([int(n) for n in numbers])
-    return total/len(numbers)
-
 def count_of_files(filename):
     sum_n = 0
     amount_n = 0
-    
+
     with open(filename, 'r') as f:
         for line in f:
             try:
@@ -23,8 +15,11 @@ def count_of_files(filename):
 
     return sum_n, amount_n
 
+
 if __name__ == '__main__':
 
-    print 'Testing the first function only with digits. Result: ', average('onlynumbers.txt')
+    sum_n, len_n = count_of_files('onlynumbers.txt')
+    print 'Testing only with digits. Result: ',  sum_n/len_n
+
     sum_n, len_n = count_of_files('case1.txt')
     print 'The average of numbers is:', sum_n/len_n
